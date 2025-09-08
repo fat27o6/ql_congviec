d7def9c8d3d34f7f71e2c9039388947852ee2e34
fat27o6 <r.mx2.6x6@gmail.com>
2025-09-08 21:00:33 +0700

    Update axios.js

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
-const instance = axios.create({ baseURL: import.meta.env.VITE_API_BASE || 'http://localhost:8080/api' })
+const instance = axios.create({ baseURL: import.meta.env.VITE_API_BASE })
 instance.interceptors.request.use(cfg => {
   const token = localStorage.getItem('token')
   if (token) cfg.headers.Authorization = `Bearer ${token}`
