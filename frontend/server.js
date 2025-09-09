5a3fef91906c650c3600d444d9dd89cf170d2d49
fat27o6 <r.mx2.6x6@gmail.com>
2025-09-09 21:18:40 +0700

    Update server.js

--- frontend/server.js
+++ frontend/server.js
@@ -9,8 +9,8 @@ const app = express()
 
 app.use(express.static(path.join(__dirname, "dist")))
 
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "dist/index.html"))
+app.use((req, res) => {
+  res.sendFile(path.join(__dirname, 'dist/index.html'))
 })
 
 const port = process.env.PORT || 10000
